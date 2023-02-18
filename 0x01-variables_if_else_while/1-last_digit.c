6bbc2bdcb2ed20fca80bb96fe5cb68e64e797c6e
mutlyn <berlynmumbua@gmail.com>
2023-02-18 00:00:05 -0800

    The last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,47 +1,29 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <stdio.h>
-
-/* Function to get the last digit of a number */
-int getLastDigit(int num)
-{
-/* If the la print a message indicating this */
-return (abs(num) % 10);
-}
-
-/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ * Description: checks on the last digit
+ * Return: Always 0 (success)
+ */
 int main(void)
 {
-int n;
+	int n, m;
 
-/* Generate a random number between -2147483647 and 2147483647 
- *fdf7
- *dfd
-*/
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-
-/* Print the number and its last digit */
-printf("Last digit of %d is %d ", n, getLastDigit(n));
-
-/* Determine less than 6 and not 0, or 0 */
-if (getLastDigit(n) > 5)
-{
-/* If the last digit is greater than 5, print a message indicating this */
-printf("and is greater than 5\n");
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	m = n % 10;
+	if (m > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, m);
+	}
+	else if (m < 6 && m != 0)
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, m);
+	}
+	else
+	{
+		printf("Last digit of %d is %d and is 0\n", n, m);
+	}
+	return (0);
 }
-else if
-(getLastDigit(n) == 0)
-{
-/* If the last digit is 0, print a message indicating this */
-printf("and is 0\n");
-}
-else
-{
-/* If the la print a message indicating this */
-printf("and is less than 6 and not 0\n");
-}
-/* If the la print a message indicating this */
-return (0);
-}
-
