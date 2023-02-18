f603bd7b7da39bc0b7a0ea6271e23bb1ea8b1548
mutlyn <berlynmumbua@gmail.com>
2023-02-17 19:13:49 -0800

    The last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -5,6 +5,7 @@
 /* Function to get the last digit of a number */
 int getLastDigit(int num)
 {
+/* If the la print a message indicating this */
 return (abs(num) % 10);
 }
 
@@ -13,30 +14,34 @@ int main(void)
 {
 int n;
 
-/* Generate a random number between -2147483647 and 2147483647 */
+/* Generate a random number between -2147483647 and 2147483647 
+ *fdf7
+ *dfd
+*/
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 
 /* Print the number and its last digit */
 printf("Last digit of %d is %d ", n, getLastDigit(n));
 
-/* Determine whether the last digit is greater than 5, less than 6 and not 0, or 0 */
+/* Determine less than 6 and not 0, or 0 */
 if (getLastDigit(n) > 5)
 {
 /* If the last digit is greater than 5, print a message indicating this */
 printf("and is greater than 5\n");
 }
-else if (getLastDigit(n) == 0)
+else if
+(getLastDigit(n) == 0)
 {
 /* If the last digit is 0, print a message indicating this */
 printf("and is 0\n");
 }
 else
 {
-/* If the last digit is less than 6 and not 0, print a message indicating this */
-printf("and is less than 6 and not 0, -%d\n", getLastDigit(n));
+/* If the la print a message indicating this */
+printf("and is less than 6 and not 0\n");
 }
-
+/* If the la print a message indicating this */
 return (0);
 }
 
