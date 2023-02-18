776bc8babe93492d90c085e748e0c3ef3a83215e
mutlyn <berlynmumbua@gmail.com>
2023-02-17 18:58:57 -0800

    The last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,35 +1,34 @@
-#include <stdlib.h> // Include standard library for functions such as srand() and rand()
-#include <time.h> // Include time library for time-related functions
-#include <stdio.h> // Include standard input/output library for printf() function
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
 
 /* Function to get the last digit of a number */
-int getLastDigit(int num) // A function that receives a number and returns its last digit
-{
-return (abs(num) % 10); // Return the absolute value of the number modulo 10 (i.e., the remainder when the number is divided by 10)
+int getLastDigit(int num) {
+    return (abs(num) % 10);
 }
 
 /* betty style doc for function main goes there */
-int main(void) // Main function
-{
-int n; // Declare an integer variable n
+int main(void) {
+    int n;
 
-srand(time(0)); // Seed the random number generator with the current time
-n = rand() - RAND_MAX / 2; // Generate a random number and assign it to n
+    /* Generate a random number between -2147483647 and 2147483647 */
+    srand(time(0));
+    n = rand() - RAND_MAX / 2;
 
-printf("Last digit of %d is %d ", n, getLastDigit(n)); // Print the message and the value of n and its last digit
+    /* Print the number and its last digit */
+    printf("Last digit of %d is %d ", n, getLastDigit(n));
 
-if (getLastDigit(n) > 5) // Check if the last digit of n is greater than 5
-{
-printf("and is greater than 5\n"); // If true, print this message
-}
-else if (getLastDigit(n) == 0) // Check if the last digit of n is 0
-{
-printf("and is 0\n"); // If true, print this message
-}
-else // If the last digit of n is less than 6 and not 0
-{
-printf("and is less than 6 and not 0\n"); // Print this message
-}
+    /* Determine whether the last digit is greater than 5, less than 6 and not 0, or 0 */
+    if (getLastDigit(n) > 5) {
+        /* If the last digit is greater than 5, print a message indicating this */
+        printf("and is greater than 5\n");
+    } else if (getLastDigit(n) == 0) {
+        /* If the last digit is 0, print a message indicating this */
+        printf("and is 0\n");
+    } else {
+        /* If the last digit is less than 6 and not 0, print a message indicating this */
+        printf("and is less than 6 and not 0\n");
+    }
 
-return (0); // Exit the program
+    return (0);
 }
