60c5db481f13ee433f715b3b326a517b3b295f1b
mutlyn <berlynmumbua@gmail.com>
2023-02-23 00:30:47 -0800

    10x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,17 +1,19 @@
 #include "main.h"
+
 /**
- * print_alphabet_x10 - prints alphabets in lowercase 10 times.
- * Return:On success 1
+ * print_alphabet_x10 - prints the lowercase alphabet 10 times followed by a new line
  */
 void print_alphabet_x10(void)
 {
-	char i,k;
-	  for (k = 0; k < 10; k++)
-	  {
-		  for (i = 'a'; i <= 'z'; i++)
-		  {
-			   _putchar(i);
-		  }
-		  putchar('\n');
-	  }
+    char letter;
+    int i;
+
+    for (i = 0; i < 10; i++)
+    {
+        for (letter = 'a'; letter <= 'z'; letter++)
+        {
+            _putchar(letter);
+        }
+        _putchar('\n');
+    }
 }
