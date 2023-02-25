bffda678a1394eb8f1a254275c982da8d485d498
mutlyn <berlynmumbua@gmail.com>
2023-02-25 02:58:00 -0800

    add two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+
+/**
+
+ * add - Adds two integers and returns
+
+ * the result.
+
+ *
+
+ *  number one.
+
+ * number two.
+
+ *
+
+ * Return: Add of number one and number two.
+
+ */
+
+int add(int a, int b)
+
+{
+
+        return (a + b);
+
+}
