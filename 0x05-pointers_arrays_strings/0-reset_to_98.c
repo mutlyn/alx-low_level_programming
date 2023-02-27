2bd8355cec073481ccaa876f555c3617826aa016
mutlyn <berlynmumbua@gmail.com>
2023-02-27 12:08:59 -0800

    reset

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,8 +2,9 @@
 /**
  * reset_to_98 - it is a function that takes a pointer to an integer as a parameter
  * updates the value it points to to 98
- * n:is input
+ * @n:is input
  * Return n
  */
-void reset_to_98(int *n)
-		{*n = 98;}
+void reset_to_98(int*n)
+		{*n=98;
+		}
