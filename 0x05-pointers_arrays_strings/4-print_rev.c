eca3bd6110dd1f317c690038094a5de3e759bcb4
mutlyn <berlynmumbua@gmail.com>
2023-02-27 13:17:13 -0800

    prints in reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,15 +1,15 @@
 #include "main.h"
 /*
- * print_rev -prints in  reverse
- * @s: string
+ * print_rev -prints a string in reverse
+ * @s:A pointer to the string to print.
  * return: 0
  */
 void print_rev(char *s)
 {
 	int longi = 0;
 	int o;
+
 	while (*s != '\0')
-	
 	{
 		longi++;
 		s++;
