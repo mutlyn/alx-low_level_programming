e117ae28679719e26dbe78d189a2dc0c0da68a1f
mutlyn <berlynmumbua@gmail.com>
2023-02-27 13:19:27 -0800

    prints in reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -2,6 +2,7 @@
 /*
  * print_rev -prints a string in reverse
  * @s:A pointer to the string to print.
+ * Description: This function takes a pointer to a string and prints each character in reverse order, followed by a newline.
  * return: 0
  */
 void print_rev(char *s)
