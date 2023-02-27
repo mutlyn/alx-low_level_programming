5760c3cd5c25ce35e5228c397fce3bceb3e0eb1e
mutlyn <berlynmumbua@gmail.com>
2023-02-27 12:31:34 -0800

    This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _strlen - returns length of the string
+ * @s: string
+ * Return: length
+ */
+int _strlen(char *s)
+{	int longi = 0;
+		while (*s != '\0')
+				{
+					longi++;
+						s++;
+				}
+		return (longi);
+}
