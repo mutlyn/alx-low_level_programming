403048cbe4ec1d241f2d19007b5f3a27093e1bce
mutlyn <berlynmumbua@gmail.com>
2023-03-02 07:20:38 -0800

    print diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * print_diagonal - draws a diagonal line on the terminal
- * @n: number of times the character 
+ * print_diagonal - draws a diagonal line on a terminal
+ * @n: number of times the character \ should be printed
  */
 
 void print_diagonal(int n)
